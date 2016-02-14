1d294a2a3b941a192bef070e29c34adfa6657f11
simonCor <sschwarz@Simons-MacBook-Air.fritz.box>
2016-02-14 19:47:41 +0100

    Changed stream socket to UDP

--- kfs_socket.py
+++ kfs_socket.py
@@ -1,11 +1,11 @@
 import socket
 
 class Kfs_socket():
-	def __init__(self, host, port, filename = "video.h264", family = socket.AF_INET, 
+	def __init__(self, host, port, filename = "video.h264", family = socket.AF_INET,
 			type = socket.SOCK_STREAM, proto = 0):
 		self.filename = filename
 		self.filehandle = open(str(self.filename), "w")
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket = socket.socket(family, type)
 		self.socket.connect((host, port))
 
 	def __del__(self):
