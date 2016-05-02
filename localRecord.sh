fdbf615312acfad246c99dd4a7a6718c9cc6cddb
simonCor <sschwarz@Simons-Air.fritz.box>
2016-05-02 22:56:45 +0200

    Added raspistill

--- localRecord.sh
+++ localRecord.sh
@@ -1,2 +1,3 @@
 myDate=`date +'%Y-%m-%d_%H_%M'`
-raspivid -o /home/pi/$myDate.h264 -n -t 1200000
+#raspivid -o /home/pi/$myDate.h264 -n -t 1200000
+raspistill -vf -hf -tl 500 -o /home/pi/camera/$myDate.jpg
