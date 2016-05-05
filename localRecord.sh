8ba49326a4f5584c95a9ff9c366fb8110e0076b8
Simon Schwarz <simon.schwarz@fobg.de>
2016-05-05 21:59:49 +0200

    Added some parameters to raspistill

--- localRecord.sh
+++ localRecord.sh
@@ -1,3 +1,5 @@
-myDate=`date +'%Y-%m-%d_%H_%M'`
+myDate=`date +'%Y-%m-%d_%H_%M_%S'`
+myFolder=/home/pi/camera/$myDate
+mkdir $myFolder
 #raspivid -o /home/pi/$myDate.h264 -n -t 1200000
-raspistill -vf -hf -tl 500 -o /home/pi/camera/$myDate.jpg
+raspistill -bm -t 0 -tl 500 -o $myFolder/%05d.jpg
