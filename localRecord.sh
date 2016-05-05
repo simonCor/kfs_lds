1d0080a8491a0d1bcf10e387f563e53d08439944
simonCor <sschwarz@Simons-MacBook-Air.fritz.box>
2016-05-05 22:21:07 +0200

    Added shutter speed 2

--- localRecord.sh
+++ localRecord.sh
@@ -2,4 +2,5 @@ myDate=`date +'%Y-%m-%d_%H_%M_%S'`
 myFolder=/home/pi/camera/$myDate
 mkdir $myFolder
 #raspivid -o /home/pi/$myDate.h264 -n -t 1200000
-raspistill -bm -t 0 -tl 500 -o $myFolder/%05d.jpg
+#-ex antishake - Not used because may influence -ss
+raspistill -bm -t 0 -tl 500 -ss 2 -o $myFolder/%05d.jpg
